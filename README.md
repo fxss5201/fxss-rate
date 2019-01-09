08ac927b98f80281722fc91d28d09ec2511951f3
fanyushun <723107234@qq.com>
2019-01-09 16:25:21 +0800

    V0.0.1版本 代码重构及新功能添加

--- README.md
+++ README.md
@@ -5,16 +5,19 @@
 ![](https://img.shields.io/badge/license-MIT-blue.svg)
 
 ## 介绍 ##
+
 fxss-rate评分插件是基于jQuery的插件，支持全星、半星、小数星、未选中星等多种显示样式，并且支持评分操作，不过操作评分的时候仅支持全星评价。
 demo地址：[http://www.fxss5201.cn/project/plugin/fxss-rate/](http://www.fxss5201.cn/project/plugin/fxss-rate/)
 
-## 资源 ##
+## 版本 ##
 
-首先需要引入样式文件：
+`0.0.1`版本。
 
-```HTML
-<link rel="stylesheet" href="fxss-rate.css">
-```
+`beta`版：
+
+1. [文档](https://github.com/fxss5201/fxss-rate/wiki/beta-%E7%89%88)
+
+## 使用 ##
 
 基于jQuery，需要引入jQuery文件：
 
@@ -22,19 +25,11 @@ demo地址：[http://www.fxss5201.cn/project/plugin/fxss-rate/](http://www.fxss5
 <script src="js/jquery-1.11.3.js"></script>
 ```
 
-渲染星星使用的是阿里矢量图库的svg字体，所以还需要引入字体文件：
+本版本使用和之前版本有点区别，需要将`fxss-rate`文件夹复制到您的项目目录中（由于`css`和`js`的压缩文件都在其中，所以使用时请根据使用情况删除源码或压缩码），只需要在页面中引入js文件：
 
 ```HTML
-<script src="fxss-rate-iconfont.js"></script>
+<script src="fxss-rate/rate.js"></script>
 ```
-
-接下来引入我们的评分插件：
-
-```HTML
-<script src="fxss-rate.js"></script>
-```
-
-## 使用 ##
 
 在使用的时候，需要一个容器，例如：
 
@@ -48,7 +43,7 @@ demo地址：[http://www.fxss5201.cn/project/plugin/fxss-rate/](http://www.fxss5
 $("#rateBox").rate();
 ```
 
-上面这种事最简单的使用，所有的参数都会使用默认参数。也可以自有配置参数，比如将其设置为可操作的并且增加点击回调函数：
+上面这种是最简单的使用，所有的参数都会使用默认参数。也可以自由配置参数，比如将其设置为可操作的并且增加点击回调函数：
 
 ```JavaScript
 $("#rateBox").rate({
@@ -59,21 +54,25 @@ $("#rateBox").rate({
 });
 ```
 
+`0.0.1`版本新增方法`change`，使用方式和`rate`一致，主要用于渲染出来的星级进行渲染（必须在`rate`之后使用）。
+
 ## 参数 ##
 
-|参数名|类型|默认值|描述|
-|----|----|----|----|
-|length|Number|5|初始长度，即渲染几颗星。|
-|value|Number|3.5|选中星星的个数，介于0和length之间，如果想要显示半颗行或者小数星，还需要设置相应的half/decimal，否则仅会显示整数部分|
-|half|Boolean|true|是否显示半星（仅做显示处理）|
-|decimal|Boolean|true|是否根据小数来显示小数星（仅做显示处理，优先级高于half）|
-|readonly|Boolean|true|是否只读，如果设置为只读，则设置callback回调函数也无用|
-|text|Boolean|true|是否显示对应的文字描述|
-|textList|Array|['极差', '差', '中等', '好', '非常好']|文字描述，可以自己设置，建议与length保持整除关系|
-|theme|String|'#FFB800'|主题色|
-|size|String|'20px'|星星以及文字描述的大小|
-|gutter|String|'3px'|两颗星星之间的距离/2，原理是设置margin: 0 gutter;|
-|selectClass|String|'fxss_rate_select'|星星选中的样式class名称，可以在此class中设置自定义样式|
-|incompleteClass|String|''|星星未完全选中的样式名称，主要针对半星和小数星|
-|customClass|String|''|自定义class，会添加在fxss_rate_wrapper标签上面，可以利用css权重来重置css样式|
-|callback|Function||星星点击的回调函数，参数object包含当前的id、点击的星星的index|
+|参数名|类型|默认值|何时添加|描述|
+|----|----|----|----|----|
+|type|Number|0|`0.0.1`|星级的渲染方式，0：`svg`，1：`Font class`，2：`Unicode`|
+|length|Number|5|`beta`|初始长度，即渲染几颗星。|
+|value|Number|3.5|`beta`|选中星星的个数，介于0和`length`之间，如果想要显示半颗星或者小数星，还需要设置相应的`half/decimal`，否则仅会显示整数部分|
+|half|Boolean|`true`|`beta`|是否显示半星（仅做显示处理）|
+|decimal|Boolean|`true`|`beta`|是否根据小数来显示小数星（仅做显示处理，优先级高于`half`）|
+|readonly|Boolean|`true`|`beta`|是否只读，如果设置为只读，则设置`callback`回调函数也无用|
+|hover|Boolean|`false`|`0.0.1`|鼠标移到星级上是否显示当前的`value`值，此版本使用简单的标签`title`表示|
+|text|Boolean|`true`|`beta`|是否显示对应的文字描述|
+|textList|Array|['极差', '差', '中等', '好', '非常好']|`beta`|文字描述，可以自己设置，建议与`length`保持整除关系|
+|theme|String|'#FFB800'|`beta`|主题色|
+|size|String|'20px'|`beta`|星星以及文字描述的大小|
+|gutter|String|'3px'|`beta`|两颗星星之间的距离/2，原理是设置`margin: 0 gutter;`|
+|selectClass|String|'rate_select'|`beta`|星星选中的样式class名称，可以在此class中设置自定义样式|
+|incompleteClass|String|''|`beta`|星星未完全选中的样式名称，主要针对半星和小数星|
+|customClass|String|''|`beta`|自定义class，会添加在`rate_wrapper`标签上面，可以利用css权重来重置css样式|
+|callback|Function||`beta`|星星点击的回调函数，参数object包含当前的DOM元素、点击的星星的index|
---
+++ fxss-rate/rate.js
@@ -0,0 +1,216 @@
+/** 
+ * fxss-rate评分插件是基于jQuery的插件，支持全星、半星、小数星、未选中星等多种显示样式，并且支持评分操作，不过操作评分的时候仅支持全星评价。
+ * time：2018-09-07
+ * by 樊小书生: http://www.fxss5201.cn/
+ * github: https://github.com/fxss5201/fxss-rate
+ */
+;
+(function(){
+    var options;
+    $.fn.rateDefaults = {
+        version: '0.0.1', // 版本号
+        type: 0, // 星级的渲染方式，0：svg  1：Font class  2：Unicode，为兼容以前版本，默认采用svg渲染
+        length: 5, // 初始长度,即渲染几颗星星
+        value: 3.5, // 选中星星的个数，介于0和length之间，如果想要显示半颗行或者小数星，还需要设置相应的half/decimal，否则仅会显示整数部分
+        half: true, // 是否显示半星（仅做显示处理）
+        decimal: true, // 是否根据小数来显示小数星（仅做显示处理，优先级高于half）
+        readonly: true, // 是否只读
+        hover: false, // 鼠标移到星级上是否显示当前的value值
+        text: true, // 是否显示对应的文字描述
+        textList: ['极差', '差', '中等', '好', '非常好'], // 文字描述
+        theme: '#FFB800', // 主题色
+        size: '20px', // 星星以及文字描述的大小
+        gutter: '3px', // 两颗星星之间的距离/2
+        selectClass: 'rate_select', // 星星选中的样式class名称，可以在此class中设置自定义样式
+        incompleteClass: '', // 星星未完全选中的样式名称，主要针对半星和小数星
+        customClass: '', // 自定义class
+        validate: function(){
+            var _this = this;
+            if(_this.value > _this.length){
+                alert('value应小于等于length');
+            }
+            if(_this.textList.length > _this.length){
+                alert('textList.length应小于等于length');
+            }
+        },
+        getPath: function(){ // 获取引入js的路径
+            var jsPath = document.currentScript ? document.currentScript.src : function(){
+                var js = document.scripts,
+                    last = js.length - 1,
+                    src;
+                for(var i = last; i > 0; i--){
+                    if(js[i].readyState === 'interactive'){
+                        src = js[i].src;
+                        break;
+                    }
+                }
+                return src || js[last].src;
+            }();
+            return jsPath.substring(0, jsPath.lastIndexOf('/') + 1);
+        }(),
+        link: function(href, cssname){ // 动态获取css
+            if(!this.getPath) return;
+            var head = document.getElementsByTagName("head")[0], link = document.createElement('link');
+            var id = cssname;
+            
+            link.rel = 'stylesheet';
+            link.href = this.getPath + href;
+            link.id = id;
+            
+            if(!document.getElementById(id)){
+                head.appendChild(link);
+            }
+        },
+        script: function(src, jsname){ // 动态获取js
+            if(!this.getPath) return;
+            var head = document.getElementsByTagName("head")[0], script = document.createElement('script');
+            var id = jsname;
+            
+            script.src = this.getPath + src;
+            script.id = id;
+            
+            if(!document.getElementById(id)){
+                head.appendChild(script);
+            }
+        },
+        addEventClick: function (renderBox, type) { // 添加点击事件
+            var _this = this;
+            $(renderBox).find(".rate_box").on("click", '.rate_item', function(){
+                var $this = $(this);
+                if(_this.value == $this.index() + 1){
+                    return false;
+                }
+                _this.value = $this.index() + 1;
+                _this.render(renderBox, type);
+                var object = {
+                    dom: $(renderBox)[0],
+                    index: $this.index()
+                }
+                _this.callback(object);
+            });
+        },
+        countValue: function(){ // 将指定的value值的整数部分和小数部分区分开
+            var _this = this;
+            var returnValue;
+            if(_this.value == parseInt(_this.value)){
+                returnValue = {
+                    int: _this.value,
+                    float: ''
+                }
+            }else{
+                returnValue = {
+                    int: parseInt(_this.value),
+                    float: (parseFloat(_this.value) * 1000 - parseInt(_this.value) * 1000) / 1000
+                }
+            }
+            return returnValue;
+        },
+        countText: function(){ // 根据value获取当前对应的textList中的值
+            var _this = this,
+                curFloat = _this.value * _this.textList.length / _this.length,
+                curRound = Math.round(curFloat),
+                curIndex = 0;
+            curIndex = curRound > 0 ? curRound - 1 : curRound; // 解决value为0时的问题
+            if(_this.decimal || _this.half){
+                _this.value > parseInt(_this.value) && (_this.value > (curRound ? curRound * _this.length / _this.textList.length : _this.length / _this.textList.length)) && curIndex++; // 解决value为小数的时候
+            }
+            return _this.textList[curIndex];
+        },
+        getIconName: function(index, type){
+            var rateItemIconUnicode = ['&#xe610;', '&#xe604;', '&#xe608;', '&#xe605;', '&#xe606;', '&#xe607;', '&#xe60b;', '&#xe60a;', '&#xe60c;', '&#xe60d;', '&#xe609;'];
+            return type * 1 === 2 ? rateItemIconUnicode[index] : ('icon-stars-' + index);
+        },
+        render: function(renderBox, type){ // 渲染函数
+            var _this = this,
+                rateBoxHtml = '',
+                rateHtml = '',
+                rateHtmlTpl = '<div class="rate_wrapper ' + _this.customClass + (_this.hover ? "\" title=\"" + _this.value + "\"" : '"') + '><div class="rate_box clearfix">{{rateHtmlTpl}}</div>{{textTpl}}</div>',
+                rateItemIcon = '';
+            var rateItemStyle = (_this.theme ? 'color:' + _this.theme : '') + ';' + (_this.gutter ? 'margin: 0 ' + _this.gutter : '') + ';';
+            var rateItemIconStyle = (_this.size ? 'font-size:' + _this.size : '') + ';';
+
+            if(type * 1 === 0){
+                rateItemIcon = '<svg class="icon" style="' + rateItemIconStyle + '" aria-hidden="true"><use xlink:href="#{{iconname}}"></use></svg>';
+            }else if(type * 1 === 1){
+                rateItemIcon = '<i class="iconfont {{iconname}}" style="' + rateItemIconStyle + '"></i>';
+            }else if(type * 1 === 2){
+                rateItemIcon = '<i class="iconfont icon-rate-item" style="' + rateItemIconStyle + '">{{iconname}}</i>';
+            }
+
+            var rateItemTpl = '<div class="rate_item {{selectClass}}" style="' + rateItemStyle + '">' + rateItemIcon + '</div>';
+            var valueObj = _this.countValue();
+            for(var i = 0; i < _this.length; i++){
+                if(_this.decimal || _this.half){ // 开启小数星渲染或半星渲染
+                    if(i < valueObj.int){
+                        rateHtml = rateHtml + rateItemTpl.replace('{{selectClass}}', _this.selectClass).replace('{{iconname}}', _this.getIconName(10, type));
+                    }else if(i == valueObj.int){
+                        var index = 0;
+                        if(_this.decimal){
+                            index = valueObj.float * 10;
+                        }else if(_this.half){
+                            if(valueObj.float < 0.33){
+                                index = 0;
+                            }else if(valueObj.float > 0.66){
+                                index = 10;
+                            }else{
+                                index = 5;
+                            }
+                        }
+                        rateHtml = rateHtml + rateItemTpl.replace('{{selectClass}}', _this.incompleteClass).replace('{{iconname}}', _this.getIconName(index, type));
+                    }else{
+                        rateHtml = rateHtml + rateItemTpl.replace('{{selectClass}}', '').replace('{{iconname}}', _this.getIconName(0, type));
+                    }
+                }else{
+                    if(i < valueObj.int){
+                        rateHtml = rateHtml + rateItemTpl.replace('{{selectClass}}', _this.selectClass).replace('{{iconname}}', _this.getIconName(10, type));
+                    }else{
+                        rateHtml = rateHtml + rateItemTpl.replace('{{selectClass}}', '').replace('{{iconname}}', _this.getIconName(0, type));
+                    }
+                }
+            }
+            rateBoxHtml = rateHtmlTpl.replace('{{rateHtmlTpl}}', rateHtml);
+
+            var textStyle = (_this.theme ? 'color:' + _this.theme : '') + ';' + (_this.size ? 'font-size:' + _this.size : '') + ';';
+            rateBoxHtml = _this.text 
+                ? (_this.value > 0 
+                    ? rateBoxHtml.replace('{{textTpl}}', '<div class="rate_text" style="' + textStyle + '">' + _this.countText() + '</div>')
+                    : rateBoxHtml.replace('{{textTpl}}', '')) 
+                : rateBoxHtml.replace('{{textTpl}}', '');
+
+            $(renderBox).html(rateBoxHtml);
+
+            if(!_this.readonly){
+                _this.addEventClick(renderBox, type);
+            }
+        },
+        callback: function(object){} // 回调函数
+    };
+    
+    $.fn.rate = function(option){
+        options = $.extend({}, $.fn.rateDefaults, option);
+        options.link('rate.css', 'rateLink');
+        if(options.type * 1 === 0){
+            options.script('need/iconfont.js', 'rateIconfontSvg');
+        }else{
+            options.link('need/iconfont.css', 'rateIconfontCss');
+        }
+        this.each(function(index, element) {
+            options.validate();
+            options.render(element, options.type);
+        });
+        return this;
+    };
+
+    $.fn.change = function(option){
+        options = $.extend({}, options, option);
+        if(options.type * 1 === 0){
+            options.script('need/iconfont.js', 'rateIconfontSvg');
+        }else{
+            options.link('need/iconfont.css', 'rateIconfontCss');
+        }
+        this.each(function(index, element) {
+            options.render(element, options.type);
+        });
+        return this;
+    };
+})(jQuery);
